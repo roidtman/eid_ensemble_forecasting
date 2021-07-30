45453e6dca3c236f184520313902b53c199ad6c1
Rachel Oidtman <racheloidtman@Rachels-MacBook-Pro.local>
2021-07-30 11:00:15 -0600

    added prior-posterior figure and adjusted color scheme

--- code/produce_figure_particles_change_through_time.R
+++ code/produce_figure_particles_change_through_time.R
@@ -3,6 +3,7 @@
 
 # Supplementary Figure
 # parameter correlations and parameter values violin plots through time
+# normalized posterior - prior figures
 #=============================================================================#
 
 
@@ -415,4 +416,394 @@ for(mm in runs){
   }
 }
 
+dev.off()
+
+
+#=============================================================================#
+# Image plot with posterior versus prior
+# prior - posterior
+# for nonspatial models, look at median value for parameters
+#=============================================================================#
+
+prior_df = data.frame(k = rep(NA, length(runs)),
+                      rho = rep(NA, length(runs)),
+                      dispersion = rep(NA, length(runs)),
+                      tm_ini_1 = rep(NA, length(runs)),
+                      loc_ini_1 = rep(NA, length(runs)),
+                      tm_ini_2 = rep(NA, length(runs)),
+                      loc_ini_2 = rep(NA, length(runs)),
+                      c = rep(NA, length(runs)),
+                      sn1 = rep(NA, length(runs)),
+                      sn2 = rep(NA, length(runs)),
+                      sn3 = rep(NA, length(runs)))
+
+posterior_df = data.frame(k = rep(NA, length(runs)),
+                          rho = rep(NA, length(runs)),
+                          dispersion = rep(NA, length(runs)),
+                          tm_ini_1 = rep(NA, length(runs)),
+                          loc_ini_1 = rep(NA, length(runs)),
+                          tm_ini_2 = rep(NA, length(runs)),
+                          loc_ini_2 = rep(NA, length(runs)),
+                          c = rep(NA, length(runs)),
+                          sn1 = rep(NA, length(runs)),
+                          sn2 = rep(NA, length(runs)),
+                          sn3 = rep(NA, length(runs)))
+
+for(mm in c(1:16)){
+  
+  if(mm %in% c(1,9,13)){
+    particles = array(NA, dim = c(length(when_to_assimilate), np, 5))
+    
+    for(tt in 1:length(when_to_assimilate)){
+      eval(parse(text = paste0(
+        'particles_tmp = particles_', when_to_assimilate[tt], '_', mm
+      )))
+      
+      particles[tt,,] = as.matrix(particles_tmp)
+    } 
+    
+    # data frame wiht priors and posteriors
+    prior_df$k[mm] = median(particles[1,,1])
+    posterior_df$k[mm] = median(particles[length(when_to_assimilate),,1])
+    
+    prior_df$rho[mm] = median(particles[1,,2])
+    posterior_df$rho[mm] = median(particles[length(when_to_assimilate),,2])
+    
+    prior_df$dispersion[mm] = median(particles[1,,3])
+    posterior_df$dispersion[mm] = median(particles[length(when_to_assimilate),,3])
+    
+    prior_df$tm_ini_1[mm] = getmode(particles[1,,4])
+    posterior_df$tm_ini_1[mm] = getmode(particles[length(when_to_assimilate),,4])
+    
+    prior_df$loc_ini_2[mm] = getmode(particles[1,,5])
+    posterior_df$loc_ini_2[mm] = getmode(particles[length(when_to_assimilate),,5])
+    
+  }else if(mm %in% c(2, 10, 14)){
+    particles = array(NA, dim = c(length(when_to_assimilate), np, 7))
+    
+    for(tt in 1:length(when_to_assimilate)){
+      eval(parse(text = paste0(
+        'particles_tmp = particles_', when_to_assimilate[tt], '_', mm
+      )))
+      
+      particles[tt,,] = as.matrix(particles_tmp)
+    }
+    
+    # data frame wiht priors and posteriors
+    prior_df$k[mm] = median(particles[1,,1])
+    posterior_df$k[mm] = median(particles[length(when_to_assimilate),,1])
+    
+    prior_df$rho[mm] = median(particles[1,,2])
+    posterior_df$rho[mm] = median(particles[length(when_to_assimilate),,2])
+    
+    prior_df$dispersion[mm] = median(particles[1,,3])
+    posterior_df$dispersion[mm] = median(particles[length(when_to_assimilate),,3])
+    
+    prior_df$tm_ini_1[mm] = getmode(particles[1,,4])
+    posterior_df$tm_ini_1[mm] = getmode(particles[length(when_to_assimilate),,4])
+    
+    prior_df$loc_ini_1[mm] = getmode(particles[1,,5])
+    posterior_df$loc_ini_1[mm] = getmode(particles[length(when_to_assimilate),,5])
+    
+    prior_df$tm_ini_2[mm] = getmode(particles[1,,6])
+    posterior_df$tm_ini_2[mm] = getmode(particles[length(when_to_assimilate),,6])
+    
+    prior_df$loc_ini_2[mm] = getmode(particles[1,,7])
+    posterior_df$loc_ini_2[mm] = getmode(particles[length(when_to_assimilate),,7])
+    
+  }else if(mm %in% c(3, 11, 15)){
+    particles = array(NA, dim = c(length(when_to_assimilate), np, 9))
+    
+    for(tt in 1:length(when_to_assimilate)){
+      eval(parse(text = paste0(
+        'particles_tmp = particles_', when_to_assimilate[tt], '_', mm
+      )))
+      
+      particles[tt,,] = as.matrix(particles_tmp)
+    } 
+    
+    # data frame wiht priors and posteriors
+    prior_df$k[mm] = median(particles[1,,2])
+    posterior_df$k[mm] = median(particles[length(when_to_assimilate),,2])
+    
+    prior_df$rho[mm] = median(particles[1,,3])
+    posterior_df$rho[mm] = median(particles[length(when_to_assimilate),,3])
+    
+    prior_df$dispersion[mm] = median(particles[1,,4])
+    posterior_df$dispersion[mm] = median(particles[length(when_to_assimilate),,4])
+    
+    prior_df$tm_ini_1[mm] = getmode(particles[1,,5])
+    posterior_df$tm_ini_1[mm] = getmode(particles[length(when_to_assimilate),,5])
+    
+    prior_df$loc_ini_1[mm] = getmode(particles[1,,6])
+    posterior_df$loc_ini_1[mm] = getmode(particles[length(when_to_assimilate),,6])
+    
+    prior_df$c[mm] = median(particles[1,, 1])
+    posterior_df$c[mm] = median(particles[length(when_to_assimilate),, 1])
+    
+    prior_df$sn1[mm] = median(particles[1,,7])
+    posterior_df$sn1[mm] = median(particles[length(when_to_assimilate),,7])
+    
+    prior_df$sn2[mm] = median(particles[1,,8])
+    posterior_df$sn2[mm] = median(particles[length(when_to_assimilate),,8])
+    
+    prior_df$sn3[mm] = median(particles[1,,9])
+    posterior_df$sn3[mm] = median(particles[length(when_to_assimilate),,9])
+    
+  }else if(mm %in% c(4, 12, 16)){
+    particles = array(NA, dim = c(length(when_to_assimilate), np, 11))
+    
+    for(tt in 1:length(when_to_assimilate)){
+      eval(parse(text = paste0(
+        'particles_tmp = particles_', when_to_assimilate[tt], '_', mm
+      )))
+      
+      particles[tt,,] = as.matrix(particles_tmp)
+    }
+    
+    # data frame wiht priors and posteriors
+    prior_df$k[mm] = median(particles[1,,2])
+    posterior_df$k[mm] = median(particles[length(when_to_assimilate),,2])
+    
+    prior_df$rho[mm] = median(particles[1,,3])
+    posterior_df$rho[mm] = median(particles[length(when_to_assimilate),,3])
+    
+    prior_df$dispersion[mm] = median(particles[1,,4])
+    posterior_df$dispersion[mm] = median(particles[length(when_to_assimilate),,4])
+    
+    prior_df$tm_ini_1[mm] = getmode(particles[1,,5])
+    posterior_df$tm_ini_1[mm] = getmode(particles[length(when_to_assimilate),,5])
+    
+    prior_df$loc_ini_1[mm] = getmode(particles[1,,6])
+    posterior_df$loc_ini_1[mm] = getmode(particles[length(when_to_assimilate),,6])
+    
+    prior_df$tm_ini_2[mm] = getmode(particles[1,,7])
+    posterior_df$tm_ini_2[mm] = getmode(particles[length(when_to_assimilate),,7])
+    
+    prior_df$loc_ini_2[mm] = getmode(particles[1,,8])
+    posterior_df$loc_ini_2[mm] = getmode(particles[length(when_to_assimilate),,8])
+    
+    prior_df$c[mm] = median(particles[1,, 1])
+    posterior_df$c[mm] = median(particles[length(when_to_assimilate),, 1])
+    
+    prior_df$sn1[mm] = median(particles[1,,9])
+    posterior_df$sn1[mm] = median(particles[length(when_to_assimilate),,9])
+    
+    prior_df$sn2[mm] = median(particles[1,,10])
+    posterior_df$sn2[mm] = median(particles[length(when_to_assimilate),,10])
+    
+    prior_df$sn3[mm] = median(particles[1,,11])
+    posterior_df$sn3[mm] = median(particles[length(when_to_assimilate),,11])
+    
+  }else if(mm == 5){
+    particles = list()
+    length(particles) = length(dept_names)
+    
+    for(dd in 1:length(dept_names)){
+      if(dd != 22){
+        particles[[dd]] = array(NA, dim = c(length(when_to_assimilate_NS[[dd]]),
+                                            np, 4))
+        for(tt in 1:length(when_to_assimilate_NS[[dd]])){
+          
+          eval(parse(text = paste0(
+            'particles_tmp = particles_', dd, '_', when_to_assimilate_NS[[dd]][tt], '_', runs[mm]
+          )))
+          
+          particles[[dd]][tt,,] = as.matrix(particles_tmp)
+        }
+      }
+    }
+    
+    # median particles
+    depts_to_consider = c(1:21, 23:32)
+    
+    prior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,1]))
+    posterior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,1]))
+    
+    prior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,2]))
+    posterior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,2]))
+    
+    prior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,3]))
+    posterior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,3]))
+    
+    prior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,4]))
+    posterior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,4]))
+    
+  }else if(mm == 6){
+    particles = list()
+    length(particles) = length(dept_names)
+    
+    for(dd in 1:length(dept_names)){
+      if(dd != 22){
+        particles[[dd]] = array(NA, dim = c(length(when_to_assimilate_NS[[dd]]),
+                                            np, 5))
+        for(tt in 1:length(when_to_assimilate_NS[[dd]])){
+          
+          eval(parse(text = paste0(
+            'particles_tmp = particles_', dd, '_', when_to_assimilate_NS[[dd]][tt], '_', runs[mm]
+          )))
+          
+          particles[[dd]][tt,,] = as.matrix(particles_tmp)
+        }
+      }
+    }
+    
+    # median particles
+    depts_to_consider = c(1:21, 23:32)
+    
+    prior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,1]))
+    posterior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,1]))
+    
+    prior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,2]))
+    posterior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,2]))
+    
+    prior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,3]))
+    posterior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,3]))
+    
+    prior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,4]))
+    posterior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,4]))
+    
+    prior_df$tm_ini_2[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,5]))
+    posterior_df$tm_ini_2[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,5]))
+    
+  }else if(mm == 7){
+    particles = list()
+    length(particles) = length(dept_names)
+    
+    for(dd in 1:length(dept_names)){
+      if(dd != 22){
+        particles[[dd]] = array(NA, dim = c(length(when_to_assimilate_NS[[dd]]),
+                                            np, 8))
+        for(tt in 1:length(when_to_assimilate_NS[[dd]])){
+          
+          eval(parse(text = paste0(
+            'particles_tmp = particles_', dd, '_', when_to_assimilate_NS[[dd]][tt], '_', runs[mm]
+          )))
+          
+          particles[[dd]][tt,,] = as.matrix(particles_tmp)
+        }
+      }
+    }
+    
+    # median particles
+    depts_to_consider = c(1:21, 23:32)
+    
+    prior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,2]))
+    posterior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,2]))
+    
+    prior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,3]))
+    posterior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,3]))
+    
+    prior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,4]))
+    posterior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,4]))
+    
+    prior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,5]))
+    posterior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,5]))
+    
+    prior_df$c[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,1]))
+    posterior_df$c[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,1]))
+    
+    prior_df$sn1[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,6]))
+    posterior_df$sn1[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,6]))
+    
+    prior_df$sn2[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,7]))
+    posterior_df$sn2[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,7]))
+    
+    prior_df$sn3[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,8]))
+    posterior_df$sn3[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,8]))
+    
+  }else if(mm == 8){
+    particles = list()
+    length(particles) = length(dept_names)
+    
+    for(dd in 1:length(dept_names)){
+      if(dd != 22){
+        particles[[dd]] = array(NA, dim = c(length(when_to_assimilate_NS[[dd]]),
+                                            np, 9))
+        for(tt in 1:length(when_to_assimilate_NS[[dd]])){
+          
+          eval(parse(text = paste0(
+            'particles_tmp = particles_', dd, '_', when_to_assimilate_NS[[dd]][tt], '_', runs[mm]
+          )))
+          
+          particles[[dd]][tt,,] = as.matrix(particles_tmp)
+        }
+      }
+    }
+    
+    # median particles
+    depts_to_consider = c(1:21, 23:32)
+    
+    prior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,2]))
+    posterior_df$k[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,2]))
+    
+    prior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,3]))
+    posterior_df$rho[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,3]))
+    
+    prior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,4]))
+    posterior_df$dispersion[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,4]))
+    
+    prior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,5]))
+    posterior_df$tm_ini_1[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,5]))
+    
+    prior_df$tm_ini_2[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][1,,6]))
+    posterior_df$tm_ini_2[mm] = getmode(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,6]))
+    
+    prior_df$c[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,1]))
+    posterior_df$c[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,1]))
+    
+    prior_df$sn1[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,7]))
+    posterior_df$sn1[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,7]))
+    
+    prior_df$sn2[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,8]))
+    posterior_df$sn2[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,8]))
+    
+    prior_df$sn3[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][1,,9]))
+    posterior_df$sn3[mm] = median(sapply(depts_to_consider, function(ff) particles[[ff]][length(when_to_assimilate_NS[[ff]]),,9]))
+  }
+}
+
+
+prior_post_diff = matrix(NA, nrow = length(runs), ncol = 11)
+for(ii in 1:nrow(prior_post_diff)){
+  prior_post_diff[ii, ] = as.numeric(posterior_df[ii, ]) - as.numeric(prior_df[ii, ])
+}
+
+prior_post_diff_to_plot = prior_post_diff[,-c(4:7)]
+
+# DIVIDE BY THE PRIOR
+prior_post_diff_to_plot = prior_post_diff[,-c(4:7)]
+prior_post_diff_to_plot[,1] = prior_post_diff_to_plot[,1] / prior_df$k
+prior_post_diff_to_plot[,2] = prior_post_diff_to_plot[,2] / prior_df$rho
+prior_post_diff_to_plot[,3] = prior_post_diff_to_plot[,3] / prior_df$dispersion
+prior_post_diff_to_plot[,4] = prior_post_diff_to_plot[,4] / prior_df$c
+prior_post_diff_to_plot[,5] = prior_post_diff_to_plot[,5] / prior_df$sn1
+prior_post_diff_to_plot[,6] = prior_post_diff_to_plot[,6] / prior_df$sn2
+prior_post_diff_to_plot[,7] = prior_post_diff_to_plot[,7] / prior_df$sn3
+
+
+model_names = c('R-1-CDRs', 'R-2-CDRs', 'Rt-1-CDRs', 'Rt-2-CDRs',
+                'R-1-nonspatial', 'R-2-nonspatial', 'Rt-1-nonspatial', 'Rt-2-nonspatial',
+                'R-1-gravity', 'R-2-gravity', 'Rt-1-gravity', 'Rt-2-gravity', 
+                'R-1-radiation', 'R-2-radiation', 'Rt-1-radiation', 'Rt-2-radiation')
+param_names = c('R multiplier', 'Reporting rate', 'Overdispersion', 'Rt scalar', 
+                'location (skew normal)', 'scale (skew normal)', 'shape (skew normal)')
+
+
+pdf('../output/main_text_figures/supplement/posterior-prior_parameters_normalized.pdf', height = 5, width = 5.5)
+layout(matrix(c(1,1,1,1,1,1,2), nrow = 1))
+par(mar = c(1,1,1,0), oma = c(10,8.5,1,3))
+cols = center_data_fxn(vec_in = as.vector(prior_post_diff_to_plot), browse = F,
+                       col_less_in = 'brown1', col_greater_in = 'cornflowerblue', by_in = 0.01, add_in = 0.01, col_middle_in = 'gray95')
+image(t(prior_post_diff_to_plot), col = cols, xaxt = 'n', yaxt = 'n')
+box()
+axis(side = 1, at = seq(0, 1, length.out = ncol(prior_post_diff_to_plot)), labels = param_names, las = 2)
+axis(side = 2, at = seq(0, 1, length.out = length(runs)), labels = model_names, las = 1)
+mtext(side = 2, line = 8, text = 'Models', cex = 0.75)
+mtext(side = 1, line = 9.5, text = 'Parameters', cex = 0.75)
+par(mar = c(0,1,0,0))
+color.bar(cols, min = -1)
+axis(side = 4, at = c(-1, 1), labels = signif(c(min(prior_post_diff_to_plot, na.rm = T),
+                                                max(prior_post_diff_to_plot, na.rm = T)), 2), las = 1)
+mtext(side = 3, 'Posterior - Prior', line = -0.5, cex = 0.75)
 dev.off()
